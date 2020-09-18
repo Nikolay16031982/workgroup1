e9c686e9ee68223b484642b717d8788c720d5eb0
Nikolay16031982 <kolya.kuzmichev.82@mail.ru>
2020-09-18 16:37:32 +0300

    редактирование функций

--- todo.js
+++ todo.js
@@ -1,40 +1,60 @@
 jQuery("document").ready(()=>{
 	jQuery(".button").on("click",()=>{
 		let value = jQuery("input").val();
-		alert(value);
 		
+		toDo = addToDo(toDo, value);
+		for (let i=0;i<toDo.length;++i)
+		alert(toDo[i].name);
 	})
 });
 
 
       // Массив объектов "список дел"
       let toDo = [
-        { id: 1, status: false, name: "Сходить в магазин" },
-        { id: 2, status: false, name: "Убраться дома" },
-        { id: 3, status: false, name: "Постричь газон" },
-        { id: 4, status: false, name: "Повесить картину" },
+        { id: 1, status: false, name: "Выучить JS" },
+        { id: 2, status: false, name: "Выучить CSS" },
+        { id: 3, status: false, name: "Выучить HTML" },
+        { id: 4, status: false, name: "Выучить jQuery" },
       ];
 
       // функция добавления нового пункта в список
-      function addToDo(arr) {
-        let add = prompt("Введите название дела", "");
+      function addToDo(arr, n_ame) {		 
+		  if(!n_ame)	return;
+        //let add = prompt("Введите название дела", "");
         let newDeal = {
           id: +`${arr.length + 1}`,
           status: false,
-          name: `${add}`,
-        };
-        console.log(arr.concat(newDeal));
+          name:  n_ame,
+        };		       
         return arr.concat(newDeal);
       }
 
       // функция удаления туду элемента
-      function deleteToDo(arr) {
+      function deleteToDo(arr, i_d) {
         let count = 1;
-        let del = prompt("Введите id дела, которое хотите удалить", 0);
-        let res = arr.filter((item) => item.id != `${del}`);
+        let res = arr.filter((item) => item.id != i_d);
         for (let obj of res) {
           obj.id = count++;
-        }
-        console.log(res);
+        }      
         return res;
+      }
+	  // функция для изменения статуса туду элемента 
+	   function changeStatusToDo(arrElement, s_tatus) {
+		arrElement.status=s_tatus; 
+			return arrElement;
+      }
+	  //функция для изменения названия туду элемента
+	  function changeNameToDo(arrElement, n_ame) {
+		arrElement.name=n_ame; 
+			return arrElement;       
+      }
+	  // функция для изменения статуса туду элемента
+	   function changeStatusToDo(arr, i_d, s_tatus) {
+		let temp = arr.map(item=>if());  
+			return arrElement;
+      }
+	  //функция для изменения названия туду элемента
+	  function changeNameToDo(arrElement, i_d, n_ame) {
+		arrElement.name=n_ame; 
+			return arrElement;       
       }
