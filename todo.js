d9c0da910bc21253c6cd3ba283c9099a0f5bf0c5
Nikolay16031982 <70543849+Nikolay16031982@users.noreply.github.com>
2020-09-22 16:58:57 +0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,3 +1,22 @@
+
+window.onload = ()=>{
+	
+document.getElementById("addButton").onclick = ()=>{
+	
+let val = document.getElementById("inText").value;		
+		toDo = addToDo(toDo, val);
+		for (let i=0;i<toDo.length;++i)
+		alert(toDo[i].name);	
+ };
+ 
+document.getElementById("delId1").onclick = ()=>{	
+		
+		toDo = deleteToDo(toDo, 1);	
+ }; 
+};
+
+
+
 let toDoList = [
   { id: 1, status: false, name: "Выучить JS" },
   { id: 2, status: false, name: "Выучить CSS" },
